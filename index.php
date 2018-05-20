ce1e0086449fac63c61262b3e33172e100515b07
Matheus Vizoto <mtsvisoto@gmail.com>
2018-05-19 22:11:04 -0300

    PrimeiroRepositorio

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$variavel = substr('teste', 0,13);
+if ($variavel = 'teste') {
+	echo "teste";
+	}else{
+		echo "teste2";
+}
+
+?>
+
+<div style="background-color: #4CAF50; /* Green */ border: none;color: white; padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block; font-size: 16px;"> <?php echo "$variavel";?> 
